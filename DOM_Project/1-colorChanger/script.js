b7a784649aec530a3059fc517a235a906a2ce0bc
sachindada1 <sachinbelbase78@gmail.com>
2026-06-01 16:33:06 +0545

    project 1.1 done

---
+++ DOM_Project/1-colorChanger/script.js
@@ -0,0 +1,38 @@
+const buttons = document.querySelectorAll(".button");
+
+const body = document.querySelector("body");
+
+buttons.forEach((btn) => {
+  console.log(btn);
+  btn.addEventListener("click", (e) => {
+    console.log(e);
+    console.log(e.target);
+    switch (e.target.id) {
+      case "grey":
+        body.style.backgroundColor = e.target.id;
+        break;
+      case "white":
+        body.style.backgroundColor = e.target.id;
+        break;
+      case "blue":
+        body.style.backgroundColor = e.target.id;
+        break;
+      case "yellow":
+        body.style.backgroundColor = e.target.id;
+        break;
+      case "navy":
+        body.style.backgroundColor = e.target.id;
+        break;
+    }
+
+    // if (e.target.id === "grey") {
+    //   body.style.backgroundColor = e.target.id;
+    // } else if (e.target.id === "white") {
+    //   body.style.backgroundColor = e.target.id;
+    // } else if (e.target.id === "blue") {
+    //   body.style.backgroundColor = e.target.id;
+    // } else if (e.target.id === "yellow") {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+  });
+});
